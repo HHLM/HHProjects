6d6e4288ea3f0f3a425b077aecd151754b6c66a9
Now <now@liuxiaohuideMacBook-Pro.local>
2019-02-13 10:18:55 +0800

    add files

---
+++ HHAnimation/HHAnimation/HHLanchScreenVC.h
@@ -0,0 +1,17 @@
+//
+//  HHLanchScreenVC.h
+//  HHAnimation
+//
+//  Created by Now on 2019/1/31.
+//  Copyright © 2019 你在哪里呀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHLanchScreenVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HHAnimation/HHAnimation/HHLanchScreenVC.m
@@ -0,0 +1,32 @@
+//
+//  HHLanchScreenVC.m
+//  HHAnimation
+//
+//  Created by Now on 2019/1/31.
+//  Copyright © 2019 你在哪里呀. All rights reserved.
+//
+
+#import "HHLanchScreenVC.h"
+
+@interface HHLanchScreenVC ()
+
+@end
+
+@implementation HHLanchScreenVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
